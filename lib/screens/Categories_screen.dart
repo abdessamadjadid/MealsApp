ceedbf380dfcaee0a431d8125df1acff3e1de28d
abdessamadjadid <abdessamadjadid@gmail.com>
2020-08-09 17:48:08 -0300

    MealsApp
    Meals App using Flutter and Dart

---
+++ lib/screens/Categories_screen.dart
@@ -0,0 +1,27 @@
+import '../models/dummy_data.dart';
+import 'package:flutter/material.dart';
+import '../widgets/Category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
